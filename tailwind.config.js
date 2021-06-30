7c876909392e61778a45ffde0a8d6574b63f5e83
NanoCell Web Design <43909932+NanoCellWebDesign@users.noreply.github.com>
2021-06-30 20:17:58 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
 module.exports = {
-  mode: 'jit',
-  purge: [
-    './src/components/*.vue'
-  ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	mode: "jit",
+	purge: ["./src/components/*.vue", "./src/views/*.vue", "./src/App.vue"],
+	darkMode: true, // or 'media' or 'class'
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
